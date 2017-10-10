ffb7fc5c965d8f8cb43ccb0885da23a1d41612fa
Lizhang_Xie <lizhang.xie@gmail.com>
2017-10-10 16:29:23 -0500

    added environment variables in environment.rb

--- railsNotiflight/config/environment.rb
+++ railsNotiflight/config/environment.rb
@@ -3,3 +3,8 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+RAILS_ENV => “production”
+RACK_ENV => “production”
+RAILS_LOG_TO_STDOUT => “enabled”
+RAILS_SERVE_STATIC_FILES => “enabled”
